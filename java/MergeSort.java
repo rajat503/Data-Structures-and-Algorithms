f91891e3cd29beb17f7279ad9b98d2b3ce9fd588
Rajat Agarwal <rajat503@gmail.com>
2016-01-20 12:31:01 +0530

    added java merge sort

---
+++ java/MergeSort.java
@@ -0,0 +1,40 @@
+class MergeSort
+{
+  public static void mergeSort(int a[])
+  {
+    msort(a,0,a.length-1);
+  }
+  private static void msort(int a[], int l, int r)
+  {
+    if(l<r)
+    {
+      msort(a,l,(r+l)/2);
+      msort(a,((r+l)/2)+1,r);
+      merge(a,l,r);
+    }
+  }
+  private static void merge(int a[], int l, int r)
+  {
+    int i,lc=l,rc=((r+l)/2)+1,hc=l;
+    int helper[]=new int[a.length];
+    for(i=l;i<=r;i++)
+    {
+      helper[i]=a[i];
+    }
+    while(lc<=(r+l)/2 && rc<=r)
+    {
+      if(helper[lc]<=helper[rc])
+      {
+        a[hc++]=helper[lc++];
+      }
+      else
+      {
+        a[hc++]=helper[rc++];
+      }
+    }
+    while(lc<=(r+l)/2)
+    {
+      a[hc++]=helper[lc++];
+    }
+  }
+}
